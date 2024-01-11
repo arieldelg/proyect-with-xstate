aea7caf31133a6ca69f628764e5d6561d773d749
arieldelg <136664352+arieldelg@users.noreply.github.com>
2024-01-11 10:25:57 -0700

    updating base url

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://arieldelg.github.io/proyect-with-xstate/'
 })
